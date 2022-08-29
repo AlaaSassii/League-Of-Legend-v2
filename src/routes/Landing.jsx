f9a2e1d6b185c94f8f645ce26a8d58c0a9c6dd8d
alaasassii <alaasassijs@gmail.com>
2022-08-29 16:12:42 +0100

    adding first section in landing

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -17,6 +17,27 @@ const Landing = () => {
         <p>Lorem, ipsum dolor. <a href="">Sign Up</a></p>
       </div>
     </div>
+    <div className='categories'>
+      <h1 className='Menu'>Menu</h1>
+      <div className='container'>
+        <div><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa est nihil dicta provident, mollitia fugit! Blanditiis voluptate similique, consectetur praesentium possimus inventore consequatur reprehenderit, corrupti voluptatibus placeat dolorem vitae libero. </p></div>
+        <div><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa est nihil dicta provident, mollitia fugit! Blanditiis voluptate similique, consectetur praesentium possimus inventore consequatur reprehenderit, corrupti voluptatibus placeat dolorem vitae libero. </p></div>
+        <div><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa est nihil dicta provident, mollitia fugit! Blanditiis voluptate similique, consectetur praesentium possimus inventore consequatur reprehenderit, corrupti voluptatibus placeat dolorem vitae libero. </p></div>
+      </div>
+    </div>
+    <div className='champions-section'>
+      <div className='container'>
+      <div className='image'>
+        <img src="https://wallpaperaccess.com/full/7975136.png" alt="yone.png" />
+      </div>
+      <div className='text'>
+      <h3>Champions List</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, doloribus exercitationem pariatur assumenda nesciunt quibusdam magnam, corporis rem, recusandae eum quis quaerat dicta voluptate provident sint quidem aliquam temporibus autem.</p>
+      <a href="">Go See Champions List</a>
+      </div>
+      </div>
+    </div>
+
     </>
   )
 }
