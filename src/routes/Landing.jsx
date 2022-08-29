be2a10d0580f5d136499293a310edb82e56b112c
alaasassii <alaasassijs@gmail.com>
2022-08-29 17:38:04 +0100

    adding the style to landing page

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -37,7 +37,90 @@ const Landing = () => {
       </div>
       </div>
     </div>
+    <div className='boxes-with-images'>
+      <div className='Menu'>Menu</div>
+      <div className='container'>
+        <div className='box'>
+          <div className='image'>
+            <img src="https://www.nme.com/wp-content/uploads/2021/09/Viego-pentakill-league-of-legends-2000x1270-1.jpg" alt='image' />
+          </div>
+          <div className='text'>
+              <h3>Title </h3> 
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repudiandae, delectus iure pariatur iusto impedit odit porro voluptas voluptatem corporis, ab saepe asperiores dignissimos adipisci?</p>
+          </div>
+        </div>
 
+        <div className='box'>
+          <div className='image'>
+            <img src="https://www.nme.com/wp-content/uploads/2021/09/Pentakill-league-of-legends-2000x1270-1.jpg" alt='image' />
+          </div>
+          <div className='text'>
+              <h3>Title </h3> 
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repudiandae, delectus iure pariatur iusto impedit odit porro voluptas voluptatem corporis, ab saepe asperiores dignissimos adipisci?</p>
+          </div>
+        </div>
+
+        <div className='box'>
+          <div className='image'>
+            <img src="https://www.nme.com/wp-content/uploads/2021/09/Nightbringer-Tryndamere-League-Of-Legends-2000x1270-1.jpg" alt='image' />
+          </div>
+          <div className='text'>
+              <h3>Title </h3> 
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repudiandae, delectus iure pariatur iusto impedit odit porro voluptas voluptatem corporis, ab saepe asperiores dignissimos adipisci?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='skins-section'>
+      <h3>Our New Skins</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+      <div className="contanier">
+        <div className='box'>
+            <img src="https://preview.redd.it/rhj8icdltfj71.png?auto=webp&s=4467b0ce268ce40ba1aa828006b1ee329ea4bc1d" alt="" />
+            <div className='layer'>
+              <div className='info'>
+              <h3>PENTAKILL MORDAISER</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam dolor rem consequuntur ullam veritatis!</p>
+              <ul>
+                <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quis optio similique?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              </ul>
+              </div>
+            </div>
+        </div>
+
+        <div className='box'>
+            <img src="https://preview.redd.it/rhj8icdltfj71.png?auto=webp&s=4467b0ce268ce40ba1aa828006b1ee329ea4bc1d" alt="" />
+            <div className='layer'>
+              <div className='info'>
+              <h3>PENTAKILL MORDAISER</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam dolor rem consequuntur ullam veritatis!</p>
+              <ul>
+                <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quis optio similique?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              </ul>
+              </div>
+            </div>
+        </div>
+
+        <div className='box'>
+            <img src="https://preview.redd.it/rhj8icdltfj71.png?auto=webp&s=4467b0ce268ce40ba1aa828006b1ee329ea4bc1d" alt="" />
+            <div className='layer'>
+              <div className='info'>
+              <h3>PENTAKILL MORDAISER</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam dolor rem consequuntur ullam veritatis!</p>
+              <ul>
+                <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quis optio similique?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              </ul>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
